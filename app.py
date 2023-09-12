b4c448849c6a97788ecbad820082aee8b2dd630e
LASIC <mh.13.santos@gmail.com>
2023-09-12 09:59:05 -0300

    alembic]

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import secrets
 
 from flask_smorest import Api
+from flask_migrate import Migrate
 from flask import Flask, jsonify
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -27,22 +28,22 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = str(secrets.SystemRandom().getrandbits(128))
     jwt = JWTManager(app)
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
-        return (jsonify({"message":"O token de acesso expirou.", "error":"Token expirado."}),401)
+        return jsonify({"message":"O token de acesso expirou.", "error":"Token expirado."}),401
 
 
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
-        return (jsonify({"message":"Verificação falhou.", "error":"invalid_token"}),401)
+        return jsonify({"message":"Verificação falhou.", "error":"invalid_token"}),401
 
 
     @jwt.unauthorized_loader
     def missing_token_callback(error):
-        return (jsonify({"description":"Não possui Token de acesso.", "error":"authorization_required"}),401)
+        return jsonify({"description":"Não possui Token de acesso.", "error":"authorization_required"}),401
 
 
     @jwt.additional_claims_loader
@@ -59,10 +60,13 @@ def create_app(db_url=None):
 
     @jwt.revoked_token_loader
     def revoked_token_callback(jwt_header, jwt_payload):
-        return (jsonify({"description":"Token revogado.", "error":"token_revoked"}), 401)
+        return jsonify({"description":"Token revogado.", "error":"token_revoked"}), 401
 
-    with app.app_context():
-        db.create_all()
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return jsonify({"description":"O Token não é recente.", "error":"fresh_token_required"}), 401
+
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required, create_refresh_token, get_jwt_identity
 
 from db import db
 from models import UserModel
@@ -45,8 +45,9 @@ class UserLogin(MethodView):
         user = UserModel.query.filter(UserModel.username == user_data["username"]).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
-            acess_token = create_access_token(identity=user.id)
-            return {"acess_token": acess_token}, 200
+            acess_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"acess_token": acess_token, "refresh_token":refresh_token}, 200
         abort(401, message="Usuário ou senha incorreto.")
 
 
@@ -57,3 +58,14 @@ class UserLogout(MethodView):
         jti = get_jwt()["jti"]
         BLOCKLIST.add(jti)
         return {"message": "Acesso encerrado."}, 200
+
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"access_token":new_token}, 200
